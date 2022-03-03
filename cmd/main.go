9677323f8d03e57711bd5182ce07c0944209535b
emiliocc5 <emiliocaccia@live.com.ar>
2022-03-02 23:26:01 -0300

    added server struct and invoked from main

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"github.com/emiliocc5/online-store-api/internal/services"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	s := services.New()
+
+	r := s.Router()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"data": "hello world"})
+	})
+
+	s.Run()
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/emiliocc5/online-store-api
 
 go 1.17
+
+require github.com/gin-gonic/gin v1.7.7 // indirect
