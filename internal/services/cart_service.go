5a2f2e35582bcff45bdeb164eda7bde12d00fd5f
emilioCacciaUala <emilio.caccia@uala.com.ar>
2022-03-21 10:57:13 -0300

    added minor changes to parse response

--- internal/services/cart_service.go
+++ internal/services/cart_service.go
@@ -30,7 +30,7 @@ func init() {
 func (c *CartServiceImpl) AddProduct(productId, clientId int) error {
 	errAddProd := c.CartRepository.AddProductToCart(productId, clientId)
 	if errAddProd != nil {
-		fmt.Println(fmt.Sprintf("Failed trying to add product: %+v to cart to the client: %+v with error: %+v",
+		logger.Error(fmt.Sprintf("Failed trying to add product: %+v to cart to the client: %+v with error: %+v",
 			productId, clientId, errAddProd))
 		return errAddProd
 	}
@@ -42,14 +42,28 @@ func (c *CartServiceImpl) GetCart(clientId int) (response.GetCartResponse, error
 	prods, errGetCart := c.CartRepository.GetCart(clientId)
 
 	if errGetCart != nil {
+		logger.Error(fmt.Sprintf("Failed trying to get cart for the client: %+v with error: %+v",
+			clientId, errGetCart))
 		return resp, errGetCart
 	}
 
-	resp.Products = *prods
+	resp.Products = parseProducts(*prods)
 
 	return resp, nil
 }
 
-func parseProducts([]models.Product) []response.ProductResponse {
-	
+func parseProducts(dbProducts []models.Product) []response.ProductResponse {
+	var responseProducts []response.ProductResponse
+	for _, e := range dbProducts {
+		prod := response.ProductResponse{
+			Id:          e.Id,
+			Label:       e.Label,
+			Category:    e.Category.Label,
+			Type:        e.Type,
+			DownloadUrl: e.DownloadUrl,
+			Weight:      e.Weight,
+		}
+		responseProducts = append(responseProducts, prod)
+	}
+	return responseProducts
 }
